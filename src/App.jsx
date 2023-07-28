2aa27b7604b2a25a32b2c0bce6c2aa3cf8ac471c
alok96 <dwivedialok799@gmail.com>
2023-07-28 20:03:10 +0530

    Added Scroll to Top Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Cart from './components/Cart';
 import { Toaster } from 'react-hot-toast';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
       </Routes>
       <Toaster />
+      <ScrollToTop />
       <Footer />
     </Router>
   );
